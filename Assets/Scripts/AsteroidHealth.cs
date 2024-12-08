3ab5546080f6ca3cdee16548bbfa3ebca0004ea0
Evan Gentile <ewgentile@gmail.com>
2024-12-08 13:15:31 -0500

    created health script

---
+++ Assets/Scripts/AsteroidHealth.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AsteroidHealth : MonoBehaviour
+{
+    private int health = 1;
+
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PCMovement.cs
+++ Assets/Scripts/PCMovement.cs
@@ -5,7 +5,11 @@ public class PCMovement : MonoBehaviour
 
     private Camera maincam;
     private Vector3 mousepos;
-
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canfire = true; // Start with canfire being true, so shooting can begin
+    private float timer;
+    public float fireTime = 0.5f; // Time between shots
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -43,10 +47,10 @@ public class PCMovement : MonoBehaviour
         transform.position = transform.position + change;
 
         //Rotation of Triangle
-        //mousepos = maincam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        //Vector3 rotation = mousepos - transform.position;
-        //float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        mousepos = maincam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        Vector3 rotation = mousepos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
     }
 }
