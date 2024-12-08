a0807200e1d78805c504c12f5c4399155bb52581
Evan Gentile <ewgentile@gmail.com>
2024-12-08 14:50:00 -0500

    updated asteroid health script

--- Assets/Scripts/AsteroidHealth.cs
+++ Assets/Scripts/AsteroidHealth.cs
@@ -1,16 +1,55 @@
+using UnityEditor;
 using UnityEngine;
-
-public class AsteroidHealth : MonoBehaviour
-{
-    private int health = 1;
-
-
 
     
 
-    // Update is called once per frame
-    void Update()
+public class AsteroidHealth : MonoBehaviour
+{
+    //int health = Random.Range(1, 3);
+    float health2;
+    SpriteRenderer SpriteRenderer;
+
+    public GameObject Bullet;
+
+
+
+    void Start()
     {
+        health2 = Random.Range(1, 3);
+
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        health2 -= 1;
+
         
     }
+
+
+    void Update()
+    {
+        if (health2 == 3)
+        {
+            SpriteRenderer.color = Color.magenta;
+        }
+
+        if (health2 == 2)
+        {
+            SpriteRenderer.color = Color.yellow;
+        }
+
+        if (health2 == 1)
+        {
+            SpriteRenderer.color = Color.red;
+        }
+
+        if (health2 == 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+
+    }
+
 }
