7fb36f9246b9528e656efcb5d668d5ed95d37a9b
Mr-Beav3r <200599466@student.georgianc.on.ca>
2024-12-08 18:34:02 -0500

    ForTheLoveOfGodFunction

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadSceneAsync("Start Menu");
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
