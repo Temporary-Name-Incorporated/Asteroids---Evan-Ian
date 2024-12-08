0910003d4cfa9ae42986258eb79515ca8a212cac
Evan Gentile <ewgentile@gmail.com>
2024-12-08 02:43:07 -0500

    added scripts folder

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position.y = 0.2;
+        }
+           
+
+        
+    }
+}
