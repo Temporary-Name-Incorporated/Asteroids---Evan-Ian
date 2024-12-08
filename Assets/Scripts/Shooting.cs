065d5d56bc9b3a9b0126d0d73f2db85eb784d57f
Evan Gentile <ewgentile@gmail.com>
2024-12-08 12:50:59 -0500

    fixed bullet script

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -2,51 +2,23 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    private Camera maincam;
-    private Vector3 mousepos;
-    public GameObject bullet;
-    public Transform bulletTransform;
-    public bool canfire = true; // Start with canfire being true, so shooting can begin
-    private float timer;
-    public float fireTime = 0.5f; // Time between shots
+    public GameObject BulletPrefab;
+    public float BulletSpeed = 10f;
+    public Transform BulletSpawnPoint;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        maincam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // Convert mouse position to world space
-        mousepos = maincam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-
-        // Calculate the direction from the object to the mouse
-        Vector3 rotation = mousepos - transform.position;
-
-        // Calculate the rotation angle to face the mouse
-        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-
-        // Apply the rotation to the object
-        transform.rotation = Quaternion.Euler(0, 0, rotZ);
-
-        // Accumulate time for firing cooldown
-        if (!canfire)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            timer += Time.deltaTime; // Add the time elapsed since the last frame to the timer
-            if (timer >= fireTime) // If enough time has passed
-            {
-                canfire = true; // Allow firing again
-            }
+            Shoot();
         }
+    }
 
-        // Fire when the left mouse button is pressed and shooting is allowed
-        if (Input.GetMouseButton(0) && canfire)
-        {
-            canfire = false; // Disable firing until the cooldown is finished
-            timer = 0; // Reset the timer after firing
-            Instantiate(bullet, bulletTransform.position, Quaternion.identity); // Fire the bullet
-        }
+    void Shoot()
+    {
+        GameObject Bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+
+        Rigidbody2D BulletRb = Bullet.GetComponent<Rigidbody2D>();
+        BulletRb.linearVelocity = transform.up * BulletSpeed;
     }
 }
